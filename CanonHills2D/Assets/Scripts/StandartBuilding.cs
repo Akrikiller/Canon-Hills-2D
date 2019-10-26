564c4c61335bff11ae541663c0c6a5fd0a64f2b4
Johannes <johannes.lorper@gmail.com>
2019-10-26 11:43:15 +0200

    Added Standart class for buildings and some ui

---
+++ CanonHills2D/Assets/Scripts/StandartBuilding.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandartBuilding : MonoBehaviour
+{
+
+	private string id;
+	private string name;
+	private int currentLvl;
+	private int maxLvl;
+	public enum BuildingType{TOWER, WALL, ECO};
+	private BuildingType buildingType;
+	public int currentHp = 1000;
+	public int maxHp = 1000;
+
+	public StandartBuilding(string id, string name, int currentLvl, int maxLvl, BuildingType buildingType){
+		this.id = id;
+		this.name = name;
+		this.currentLvl = currentLvl;
+		this.maxLvl = maxLvl;
+		this.buildingType = buildingType;
+
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CanonHills2D/Assets/Scripts/Tower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tower : StandartBuilding 
+{
+	private enum Projectile{STANDART_PROJECTILE};
+	private Projectile projectile = Projectile.STANDART_PROJECTILE;
+
+	public Tower(string id, string name, int currentLvl, int maxLvl): base(id, name,currentLvl, maxLvl,BuildingType.TOWER) {
+    
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp =1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHp--;
+        Slider healthSlider = GameObject.Find("healthSlider").GetComponent<Slider>(); 
+    	healthSlider.value = currentHp;
+
+    }
+}
