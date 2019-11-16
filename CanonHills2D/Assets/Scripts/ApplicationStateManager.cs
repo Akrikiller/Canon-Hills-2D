6440c9d7a058e683fe24a6d6993ef8ac73d75afc
Johannes <johannes.lorper@gmail.com>
2019-11-16 18:30:24 +0100

    Added some GameState scripts

---
+++ CanonHills2D/Assets/Scripts/ApplicationStateManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationStateManager : MonoBehaviour
+{
+
+    enum ApplicationState {menue, singlePlayerGame,endOfGame};
+   
+   private ApplicationState currentApplicationState;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        setApplicationState(ApplicationState.singlePlayerGame);
+        
+    }
+
+    // Update is called once per frame
+    void setApplicationState(ApplicationState newApplicationState){
+        currentApplicationState = newApplicationState;
+    }
+   
+}
---
+++ CanonHills2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public enum GameState{start,building, shooting, waiting, transition}
+    private int currentPlayer;
+    GameState currentPhase = GameState.start;
+    private float timeToNextPhase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayer = 0;
+        startNextPhase();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToNextPhase -= Time.deltaTime;
+        if(timeToNextPhase <= 0)
+        startNextPhase();
+    }
+
+    public void startNextPhase(){
+        switch (currentPhase)
+      {
+          case GameState.start:
+          currentPlayer = 1;
+              startBuildingPhase(currentPlayer);
+              break;
+          case GameState.building:
+              startShootingPhase(currentPlayer);
+              break;
+          case GameState.shooting:
+              startTransitioningPhase(currentPlayer);
+              break;
+
+      }
+   
+        
+    }
+
+    public void startBuildingPhase(int currentPlayer){
+        timeToNextPhase = 30;
+
+    }
+    public void startTransitioningPhase(int currentPlayer)
+    {
+        timeToNextPhase= 5;
+    }
+    public void startShootingPhase(int currentPlayer){
+        timeToNextPhase = 15;
+
+    }
+}
